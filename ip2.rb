a1e6fd09aa1ec1f6b186937fcbc9fcc906b31d64
Grace Park <parkgra3@gmail.com>
2018-08-06 20:23:09 +0000

    automatically backed up by learn

--- ip2.rb
+++ ip2.rb
@@ -1,8 +1,9 @@
-fav_animal = "monkeys"
-fav_place = "Chicago"
-fav_artist = "Zaha Hadid"
+
 
 def my_fav_things
+  fav_animal = "monkeys"
+  fav_place = "Chicago"
+  fav_artist = "Zaha Hadid"
   puts "I love #{fav_animal}!"
   puts "I love #{fav_place}!"
   puts "I love #{fav_artist}!"
